c54e62be1693db7561b24911ec3af129ee0edbbf
atextenebris <kurilenkonikitta@gmail.com>
2023-07-01 16:20:45 +0300

    Добавлена система загрузки цветовой схемы, примеры цветовых схем

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { loadScheme } from './shared/functions/load-scheme.function';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MedicalQueueFront';
+  schemePath = 'assets/first.scheme.css';
+
+  ngOnInit() {
+    loadScheme(this.schemePath);
+  }
+
 }
+
+
