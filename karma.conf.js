d7d4ec89480e10006463180e867b34f6870cd20e
Andrey <108361411+WorstFr1end@users.noreply.github.com>
2023-07-10 01:58:35 +0300

    Add files via upload

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/MedicalQueueApiAngular'),
+      dir: require('path').join(__dirname, './coverage/mockup'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
